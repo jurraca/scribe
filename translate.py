21502ec5d8e73150c2109a5f197d857256766ca1
julienurraca@gmail.com <julienurraca@gmail.com>
2021-09-04 20:00:04 -0500

    add base lang option

--- translate.py
+++ translate.py
@@ -11,27 +11,29 @@ import argparse
 
 parser = argparse.ArgumentParser('translate.py', description='Send a text file to be translated to a target language.')
 parser.add_argument('filepath', type=str, help='a path to a text file.')
+parser.add_argument('base_language', type=str, help='the input language, a two letter code (e.g. "fr", "it", "es".')
 parser.add_argument('target_language', type=str, help='the output language, a two letter code (e.g. "fr", "it", "es".')
 parser.add_argument('--custom', type=str, help='a tag associated with a csv containing a Custom Vocabulary in your AWS account. See AWS docs for more.')
 
-def parse_args(parser): 
-	args = parser.parse_args()	
+def parse_args(parser):
+	args = parser.parse_args()
 	filepath = args.filepath
+	base_language = args.base_language
 	target_language = args.target_language
 	custom_vocab = args.custom 
-	return filepath, target_language, custom_vocab
+	return filepath, base_language, target_language, custom_vocab
 
-def start_job(filepath, target_language, custom_vocab): 
+def start_job(filepath, base_language, target_language, custom_vocab):
 	with open(filepath, 'r') as f:
 		text = f.read()
-	
+
 	parts = split_text(text)
-	for p in parts: 
+	for p in parts:
 		print("Text length: " + str(len(p)))
 		translate = boto3.client(service_name='translate', region_name='us-west-2', use_ssl=True)
-		result = translate.translate_text(Text=p, SourceLanguageCode="en", TargetLanguageCode=target_language) # TerminologyNames=[custom_vocab],
-		output_text = result.get('TranslatedText')	
-		output_path = target_language + "translation_output.txt"
+		result = translate.translate_text(Text=p, SourceLanguageCode=base_language, TargetLanguageCode=target_language) # TerminologyNames=[custom_vocab],
+		output_text = result.get('TranslatedText')
+		output_path = "translation.txt"
 
 		with open(output_path, "a") as out: 
 			out.write(output_text)	
@@ -48,6 +50,6 @@ def split_text(text):
 		parts.append(text)
 	return parts
 
-if __name__ == '__main__': 
-	filepath, target_language, custom_vocab = parse_args(parser) 
-	start_job(filepath, target_language, custom_vocab)
+if __name__ == '__main__':
+	filepath, base_language, target_language, custom_vocab = parse_args(parser)
+	start_job(filepath, base_language, target_language, custom_vocab)
